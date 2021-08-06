9d387546cbc0ade4735ae9951358b4dbabcca350
Prapti Devgun <praptidevgun25@gmail.com>
2021-08-06 17:44:13 +0530

    Create Rotate by 90 degree.java

---
+++ Day05/Rotate by 90 degree.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        //transpose
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j < arr[0].length; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0; i < arr.length; i++){
+            int li = 0;
+            int ri = arr[i].length - 1;
+            while(li < ri){
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+                
+                li++;
+                ri--;
+                
+            }
+        }
+        
+        display(arr);
+    }
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
