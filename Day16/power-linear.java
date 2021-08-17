7949b4b20aa19ddaf21f93b25a405481f46b9b41
Prapti Devgun <praptidevgun25@gmail.com>
2021-08-17 23:19:12 +0530

    Create power-linear.java

---
+++ Day16/power-linear.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        int xn = power(x, n);
+        System.out.println(xn);
+    }
+
+    public static int power(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        int xnm1 = power(x, n - 1);
+        int xn = x * xnm1;
+        return xn;
+    }
+
+}
