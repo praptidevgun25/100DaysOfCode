a4f94fbc7a7ca11b81c4de9b4f0c8c35537d5ce4
Prapti Devgun <praptidevgun25@gmail.com>
2021-08-22 22:35:36 +0530

    Create primeTillN.java

---
+++ Day21/primeTillN.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+        
+        for(int n=low; n<=high; n++)
+        {
+            int count = 0;
+            for(int div = 2; div * div <=n; div++)
+            {
+                if(n%div==0)
+                {
+                    count++;
+                    break;
+                }
+            }
+            if(count==0)
+            {
+                System.out.println(n);
+            }
+        }
+        
+    }
+}
