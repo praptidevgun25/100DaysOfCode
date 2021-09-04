993d79a46764533be3114cc8022e3ea9582ff509
Prapti Devgun <praptidevgun25@gmail.com>
2021-09-04 17:57:40 +0530

    Update printMazePaths_recUp.java

--- Day33/printMazePaths_recUp.java
+++ Day33/printMazePaths_recUp.java
@@ -5,30 +5,36 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        
         int n = scn.nextInt();
         int m = scn.nextInt();
-        
-        printMazePaths(0,0,n-1,m-1,"");
+        printMazePaths(1, 1, n, m, "");
     }
 
     // sr - source row
     // sc - source column
     // dr - destination row
     // dc - destination column
-    
     public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
-        if(sr == dr && sc == dc) {
+        
+        // base case
+        if(sr == dr && sc == dc){
             System.out.println(psf);
             return;
         }
         
-        if(sc < dc) {
-            printMazePaths(sr,sc+1,dr,dc,psf + 'h'); //horizontal move
+        // horizontal move
+        for(int ms = 1; ms <= dc - sc; ms++){
+            printMazePaths(sr, sc + ms, dr, dc, psf + "h" + ms);
         }
         
-        if(sr < dr) {
-            printMazePaths(sr+1,sc,dr,dc,psf + 'v'); //vertical move
+        // vertical move
+        for(int ms = 1; ms <= dr - sr; ms++){
+            printMazePaths(sr + ms, sc, dr, dc, psf + "v" + ms);
+        }
+        
+        // diagonal move
+        for(int ms = 1; ms <= dr - sr && ms <= dc - sc; ms++){
+            printMazePaths(sr + ms, sc + ms, dr, dc, psf + "d" + ms);
         }
     }
 
