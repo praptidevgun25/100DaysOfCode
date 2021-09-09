820ca27f3134511642fe95ccd69559077537e0f6
Prapti Devgun <praptidevgun25@gmail.com>
2021-09-09 22:15:39 +0530

    Create patternfibtriangle.java

---
+++ Day39/patternfibtriangle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int a=0;
+        int b=1;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+            System.out.print(a + "	");
+            int c=a+b;
+            a=b;
+            b=c;
+            }
+        System.out.println();
+    }
+}
+}
