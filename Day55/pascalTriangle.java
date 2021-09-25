95179dee09493bc5e36a3793b4864cef74ca6fe2
Prapti Devgun <praptidevgun25@gmail.com>
2021-09-25 22:34:54 +0530

    Create pascalTriangle.java

---
+++ Day55/pascalTriangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    for (int i = 0; i < n; i++) {
+        int icj = 1;
+      for (int j = 0; j <= i; j++) {
+        System.out.print(icj + "	");
+        int icjp1 = icj * (i - j) / (j + 1);
+        icj = icjp1;
+      }
+
+      System.out.println();
+    }
+
+  }
+}
