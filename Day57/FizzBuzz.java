91de0a599391e48e35ed57aef1e95e20b012ca3f
Prapti Devgun <praptidevgun25@gmail.com>
2021-09-27 17:54:42 +0530

    Create FizzBuzz.java

---
+++ Day57/FizzBuzz.java
@@ -0,0 +1,28 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i if non of the above conditions are true.
+
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(Integer.toString(i));
+            }
+        }
+        return res;
+    }
+}
