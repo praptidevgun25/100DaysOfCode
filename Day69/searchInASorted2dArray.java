b04ac0f84821440cfa81e0bb7dd50f98625a7822
Prapti Devgun <praptidevgun25@gmail.com>
2021-10-09 21:30:37 +0530

    Create searchInASorted2dArray.java

---
+++ Day69/searchInASorted2dArray.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int[][] arr = new int[n][n];
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        arr[i][j] = scn.nextInt();
+      }
+    }
+
+    int x = scn.nextInt();
+
+    /* top left corner */
+    int i = 0;
+    int j = arr[0].length - 1;
+    /* top left corner */
+
+    while (i < arr.length && j >= 0) {
+      if (x > arr[i][j]) {
+        i++;
+
+      } else if (x < arr[i][j]) {
+        j--;
+
+      } else {
+
+        System.out.println(i);
+        System.out.println(j);
+        return;
+
+      }
+    }
+
+    System.out.println("Not Found");
+  }
+
+}
